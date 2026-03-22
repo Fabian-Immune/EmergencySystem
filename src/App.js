4dfbfb5e46194a8dd70e857c609b335a1427aefa
Fabian-Immune <fabian.vega@alumni.immune.institute>
2026-03-22 02:53:30 -0600

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; 
-import EmergencyForm from './components/EmergencyForm';
+import EmergencyForm from './components/EmergencyForm.js';
 //import EmergencyList from './components/EmergencyList';
 
 function App() {
